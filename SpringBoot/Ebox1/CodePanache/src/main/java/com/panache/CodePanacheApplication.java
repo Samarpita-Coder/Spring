aa9b7927c030174263743c36de5a470e1695dcc0
Samarpita-Coder <97778822+Samarpita-Coder@users.noreply.github.com>
2022-04-27 14:04:30 +0530

    Add files via upload

---
+++ SpringBoot/Ebox1/CodePanache/src/main/java/com/panache/CodePanacheApplication.java
@@ -0,0 +1,13 @@
+package com.panache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodePanacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodePanacheApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/Ebox1/CodePanache/src/test/java/com/panache/CodePanacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodePanacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
